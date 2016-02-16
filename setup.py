0648a2ddd861400e39de08406ecfc2b936e278cc
Kevin Chavez <kjchavez@stanford.edu>
2016-02-15 22:40:04 -0800

    Updated name in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='gimage',
+setup(name='gcse_images',
       version='0.1',
       description='Lightweight utility for using Google Custom Search API '
                   'for image searches.',
-      url='http://github.com/kjchavez/gimage',
+      url='http://github.com/kjchavez/gcse-images',
       author='Kevin Chavez',
       author_email='kevin.j.chavez@gmail.com',
       license='MIT',
-      packages=['gimage'],
+      packages=['gcse_images'],
       zip_safe=False)
