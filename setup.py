e135c11c67b9d0c303edc0f5a6d8752549707ddc
Kevin Chavez <kjchavez@stanford.edu>
2016-02-15 22:38:27 -0800

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gimage',
+      version='0.1',
+      description='Lightweight utility for using Google Custom Search API '
+                  'for image searches.',
+      url='http://github.com/kjchavez/gimage',
+      author='Kevin Chavez',
+      author_email='kevin.j.chavez@gmail.com',
+      license='MIT',
+      packages=['gimage'],
+      zip_safe=False)
